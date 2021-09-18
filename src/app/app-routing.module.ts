3b6b62c2b3bf54aea866819112b920f90e523b87
sakis475 <sakis475@users.noreply.github.com>
2021-09-18 18:05:34 +0300

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,9 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  
   {
     path: '',
-    loadChildren: () =>
-      import('./login-or-register/login-or-register.module').then(
-        (m) => m.LoginOrRegisterPageModule
-      ),
-    pathMatch: 'full',
-  },
-  {
-    path: 'game',
     loadChildren: () =>
       import('./thegame/thegame.module').then((m) => m.ThegamePageModule),
     pathMatch: 'full',
